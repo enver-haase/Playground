5cbb00858507198fc40928a8422538c26cfb8db4
Enver Haase <enver.haase@infraleap.com>
2016-04-02 09:44:53 +0200

    First version for GitHub.

---
+++ JavaScriptDemo/web/mycode.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ehaase on 26.01.16.
+ */
+function calc(numA, numB) {
+    return numA+numB;
+}
+
+function showEnver(){
+    document.getElementById("enver").innerHTML = "Enver was here!";
+}
