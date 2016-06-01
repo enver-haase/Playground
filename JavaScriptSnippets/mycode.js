3312bd26c1a3a800bd58dd6bf2b456a355967f10
Enver Haase <enver@vaadin.com>
2016-06-01 15:57:07 +0200

    Moved to JS Snippets - not reall web components or such.

---
+++ JavaScriptSnippets/mycode.js
@@ -0,0 +1,12 @@
+
+function calc(numA, numB) {
+    return numA+numB;
+}
+
+function showEnver(){
+    document.getElementById("enver").innerHTML = "Enver was here!";
+}
+
+function moan(){
+    alert("Man, alas alas alas... moan moan moan...!!!");
+}
