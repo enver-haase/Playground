4ad27931666a07ab938e53ce4e92a44c8d969e76
Enver Haase <enver.haase@infraleap.com>
2016-07-11 23:28:19 +0200

    * remove unused modules, add Vaadin-enabled kick-off

---
+++ struts-examples/basic-struts-with-vaadin/src/main/java/com/infraleap/strutsapp/IndexAction.java
@@ -0,0 +1,28 @@
+
+package com.infraleap.strutsapp;
+
+import java.util.Random;
+
+public class IndexAction {
+    private String name;
+
+    static Random random = new Random();
+
+    public String execute() throws Exception {
+
+        if (random.nextDouble() > 0.5) {
+
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ struts-examples/basic-struts-with-vaadin/src/main/webapp/fail.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fail!!</title>
+</head>
+
+<body>
+<h1>Welcome To failing Struts 2!</h1>
+</body>
+</html>
