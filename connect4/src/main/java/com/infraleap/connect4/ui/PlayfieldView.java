4c2f1b94487610b107a2bf7d8257f19e8a2aa527
Enver Haase <enver.haase@infraleap.com>
2017-05-31 15:01:56 +0200

    ignores

--- connect4/src/main/java/com/infraleap/connect4/ui/PlayfieldView.java
+++ connect4/src/main/java/com/infraleap/connect4/ui/PlayfieldView.java
@@ -1,9 +1,9 @@
 package com.infraleap.connect4.ui;
 
+import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.MouseEventDetails;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Image;
 
@@ -15,6 +15,8 @@ public class PlayfieldView extends PlayfieldDesign {
     public PlayfieldView() {
         super();
 
+        clear();
+
         for (int col = 0; col < getColumns(); col++){
             for (int row = 0; row < getRows(); row++){
                 final int columnClicked = col;
@@ -24,7 +26,50 @@ public class PlayfieldView extends PlayfieldDesign {
         }
     }
 
+
+    public enum Coin{
+        RED("img/Red.png"),
+        YELLOW("img/Yellow.png"),
+        EMPTY("img/Empty.png");
+
+        private final ThemeResource value;
+        private Coin(String val) {
+            this.value = new ThemeResource(val);
+        }
+    }
+
+    public void dropCoin(Coin coin, int column){
+        if (column >= getColumns() || column < 0){
+            throw new IllegalArgumentException("Column argument out of range.");
+        }
+        for (int row = getRows()-1; row >= 0; row--){
+            Image target = (Image) getComponent(column, row);
+            if (target.getSource() == Coin.EMPTY.value){
+                target.setSource(coin.value);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Column already full.");
+    }
+
+    public void clear(){
+        forEach(new Consumer<Component>() {
+            @Override
+            public void accept(Component component) {
+                ((Image) component).setSource(Coin.EMPTY.value);
+            }
+        });
+    }
+
     private void onColumnClicked(int column){
         System.err.println("Clicked: "+column);
+        // TODO: eventbus notification of which column was clicked.
+
+        //TODO: remove
+        dropCoin(Coin.YELLOW, column);
+    }
+
+    private void checkWin(){
+        // TODO: eventbus notification if we have a winner and which color.
     }
 }
