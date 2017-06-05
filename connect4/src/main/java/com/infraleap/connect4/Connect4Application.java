38be699db3319ecb4bfe69e3339d9722188a699f
Enver Haase <enver.haase@infraleap.com>
2017-06-06 01:59:20 +0200

    Packaging issues.

--- connect4/src/main/java/com/infraleap/connect4/Connect4Application.java
+++ connect4/src/main/java/com/infraleap/connect4/Connect4Application.java
@@ -2,9 +2,16 @@ package com.infraleap.connect4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Connect4Application {
+public class Connect4Application extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Connect4Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Connect4Application.class, args);
