88bddce503f8097eb81b57fbc6ffd66059228ae1
Enver Haase <enver@vaadin.com>
2020-05-06 16:39:45 +0200

    Demo code simplified.

--- datefield-ng/src/main/java/org/vaadin/enver/MainView.java
+++ datefield-ng/src/main/java/org/vaadin/enver/MainView.java
@@ -1,6 +1,5 @@
 package org.vaadin.enver;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -15,59 +14,14 @@ import java.time.LocalDate;
 @SuppressWarnings("unused")
 public class MainView extends VerticalLayout {
 
-    // Customer supports German, English, Italian and French
     @SuppressWarnings("unused")
     public MainView() {
-        Component german = create(BetterDatePicker.I18N.GERMAN);
-        Component english = create(BetterDatePicker.I18N.ENGLISH);
-        Component italian = create(BetterDatePicker.I18N.ITALIAN);
-        Component french = create(BetterDatePicker.I18N.FRENCH);
 
-        add(german, english, italian, french);
+        BetterDatePicker betterDatePicker = new BetterDatePicker();
+        Text message = new Text(betterDatePicker.getValue() == null ? "(null)" : betterDatePicker.getValue().toString());
+
+        betterDatePicker.addValueChangeListener( v -> message.setText(v == null ? "(null)" : v.toString()) );
+
+        add(betterDatePicker, message);
     }
-
-
-    private Component create(BetterDatePicker.I18N i18n) {
-
-        VerticalLayout vl = new VerticalLayout();
-
-        Text message = new Text("");
-
-        BetterDatePicker betterDatePicker = new BetterDatePicker(i18n);
-
-        betterDatePicker.addValueChangeListener(event -> {
-            LocalDate selectedDate = event.getValue();
-            showDateResults(message, betterDatePicker, selectedDate);
-        });
-
-        // initially show something
-        showDateResults(message, betterDatePicker, betterDatePicker.getValue());
-
-        // application hack to make our listener fire
-        betterDatePicker.setValue(LocalDate.MIN);
-        betterDatePicker.setValue(LocalDate.now());
-
-        vl.add(betterDatePicker, message);
-
-        return vl;
-    }
-
-    private void showDateResults(Text message, BetterDatePicker betterDatePicker, LocalDate selectedDate) {
-        if (selectedDate != null) {
-            int weekday = selectedDate.getDayOfWeek().getValue() % 7;
-            String weekdayName = betterDatePicker.getI18n().getWeekdays()
-                    .get(weekday);
-
-            int month = selectedDate.getMonthValue() - 1;
-            String monthName = betterDatePicker.getI18n().getMonthNames()
-                    .get(month);
-
-            message.setText("Day of week: " + weekdayName + "\nMonth: "
-                    + monthName);
-        } else {
-            message.setText("No date is selected");
-        }
-    }
-
-
 }
