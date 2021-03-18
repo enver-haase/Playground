261e15f652c9ef3b60504e4b9097e99f6ed08f16
Enver Haase <enver@vaadin.com>
2021-03-18 12:31:24 +0100

    simple alternative to CSS Media Queries.

--- core-supply-sophistication/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ core-supply-sophistication/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,10 +1,12 @@
 package com.example.application.views.helloworld;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
@@ -29,6 +31,13 @@ public class HelloWorldView extends HorizontalLayout {
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
+
+        Page page = UI.getCurrent().getPage();
+        page.addBrowserWindowResizeListener(
+                event -> {
+                    sayHello.getElement().setVisible(event.getWidth() >= 500);
+                }
+        );
     }
 
 }
