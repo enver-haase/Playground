6411329973e8a40e1918cf40e97a4c2895017d0f
Enver Haase <enver@vaadin.com>
2021-03-23 19:26:25 +0100

    Craziness with a 'blocking' dialog.

--- core-supply-sophistication/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ core-supply-sophistication/src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,7 +1,8 @@
 package com.example.application.views.helloworld;
 
-//import com.infraleap.animatecss.Animated;
+import com.example.application.views.main.MainView;
 import com.example.application.views.styling.StylingView;
+import com.infraleap.animatecss.Animated;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -9,56 +10,51 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.*;
-import com.example.application.views.main.MainView;
-import com.vaadin.flow.server.RouteRegistry;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.RouteConfiguration;
+
+import java.util.function.Consumer;
 
 @CssImport("./views/helloworld/hello-world-view.css")
 @Route(value = "hello", layout = MainView.class)
 @RouteAlias(value = "", layout = MainView.class)
 @PageTitle("Hello World")
-public class HelloWorldView extends HorizontalLayout implements AfterNavigationObserver/*implements Animated*/ {
+public class HelloWorldView extends HorizontalLayout implements Animated {
 
-    private TextField name;
-    private Button sayHello;
+    private final TextField name;
+    private final Button sayHello;
 
     public HelloWorldView() {
-        //animate(Animation.ROTATE_IN);
+        animate(Animation.ROTATE_IN);
+
 
         addClassName("hello-world-view");
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         add(name, sayHello);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-
-
+                    Consumer<Boolean> delayedCode = b -> Notification.show("Hello " + name.getValue() + ". You said " + (b ? "yes" : "no"));
+                    YesNoDecider.yesNo("Yes or No? Decide now.", delayedCode);
+                }
+        );
 
         add(new Button("Enable 'styling' Route", event -> {
+            //noinspection unchecked
             RouteConfiguration.forSessionScope().setRoute("styling", StylingView.class, MainView.class);
         }));
-        add(new Button("Disable 'styling' Route", event -> {
-            RouteConfiguration.forSessionScope().removeRoute("styling");
-        }));
-
-
+        add(new Button("Disable 'styling' Route", event -> RouteConfiguration.forSessionScope().removeRoute("styling")));
 
 
         sayHello.addClassName("color-by-width");
 
         Page page = UI.getCurrent().getPage();
         page.addBrowserWindowResizeListener(
-                event -> {
-                    sayHello.getElement().setVisible(event.getWidth() >= 500);
-                }
+                event -> sayHello.getElement().setVisible(event.getWidth() >= 500)
         );
-    }
-
-
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-        System.out.println(System.identityHashCode(this));
     }
 }
---
+++ core-supply-sophistication/src/main/java/com/example/application/views/helloworld/YesNoDecider.java
@@ -0,0 +1,65 @@
+package com.example.application.views.helloworld;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.server.Command;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+
+public class YesNoDecider {
+    abstract static class MyCommand implements Command {
+        volatile boolean done;
+    }
+
+    // to be called from within a UI's thread
+    public static void yesNo(String question, Consumer<Boolean> consumer) {
+
+        Dialog dialog = new Dialog();
+
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        dialog.add(new Span(question));
+        dialog.add(new Button("yes", e -> future.complete(true)));
+        dialog.add(new Button("no", e -> future.complete(false)));
+        dialog.setModal(true);
+        dialog.setCloseOnEsc(false);
+        dialog.setCloseOnOutsideClick(false);
+        dialog.open();
+
+        // Command will be run in the UI thread eventually
+        final MyCommand command = new MyCommand() {
+            @Override
+            public void execute() {
+                if (future.isDone()) {
+                    try {
+                        consumer.accept(future.get());
+                        done = true;
+                    } catch (InterruptedException | ExecutionException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        UI ui = UI.getCurrent(); // in the UI thread, get the UI instance
+        new Thread(() -> { // worker thread trying to let the UI thread run the command as soon as the required user-input boolean if there
+            while (!command.done) { // repeat until it is finally done.
+                try {
+                    ui.access(command).get(); // and use the UI instance in a child thread, block this thread until the UI thread tried another time.
+                    // network usage: make sure that there is no polling set so that we only piggy-back upon the client-server communications that take place after some user action such as a click.
+                } catch (InterruptedException | ExecutionException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+        // functional identity mapping with a little side effect
+        future.thenApply(value -> {
+            dialog.close();
+            return value;
+        });
+    }
+}
