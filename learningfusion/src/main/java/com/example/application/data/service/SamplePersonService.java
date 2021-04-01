d97e57eb5f92d28c640508511618c15a0937433b
Enver Haase <enver@vaadin.com>
2021-04-01 12:44:37 +0200

    add a learning project

---
+++ learningfusion/src/main/java/com/example/application/data/service/SamplePersonService.java
@@ -0,0 +1,26 @@
+package com.example.application.data.service;
+
+import com.example.application.data.entity.SamplePerson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.vaadin.artur.helpers.CrudService;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+import javax.annotation.Nullable;
+
+@Service
+public class SamplePersonService extends CrudService<SamplePerson, Integer> {
+
+    private SamplePersonRepository repository;
+
+    public SamplePersonService(@Autowired SamplePersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected SamplePersonRepository getRepository() {
+        return repository;
+    }
+
+}
