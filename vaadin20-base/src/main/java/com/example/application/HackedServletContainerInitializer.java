b4985752d4e21e76670cecf27279f08deacc34c3
Enver Haase <enver@vaadin.com>
2021-06-16 18:45:25 +0200

    Try to use a new ServletContainerInitializer for a minimal Vaadin20 launch. Entirely incomplete. But Pax-Web in Karaf now deploys without coughing up. Still, 403 error when trying to use this so something is missing still.

---
+++ vaadin20-base/src/main/java/com/example/application/HackedServletContainerInitializer.java
@@ -0,0 +1,21 @@
+package com.example.application;
+
+import com.vaadin.flow.di.LookupInitializer;
+import com.vaadin.flow.server.startup.LookupServletContainerInitializer;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.*;
+
+public class HackedServletContainerInitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.err.println("XXXXXXXX IT WORKS");
+        System.out.println("XXXXXXXX IT WORKS");
+
+        List<Class<?>> collection = Arrays.asList(new Class[]{LookupInitializer.class});
+        Set<Class<?>> classes = new HashSet<>(collection);
+        new LookupServletContainerInitializer().onStartup(classes, ctx);
+    }
+}
