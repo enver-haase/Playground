74f0c25eee1827a3537b16bea82029a0081413cc
Enver Haase <enver@vaadin.com>
2021-08-17 13:10:05 +0200

    Add TODO project to match
    https://vaadin.com/docs/latest/fusion/tutorials/quick-start-tutorial

---
+++ vaadin-todo/frontend/views/todo/todo-view.ts
@@ -0,0 +1,9 @@
+import { customElement, html } from 'lit-element';
+import { View } from '../../views/view';
+
+@customElement('todo-view')
+export class TodoView extends View {
+  render() {
+    return html`<div>Content placeholder</div>`;
+  }
+}
