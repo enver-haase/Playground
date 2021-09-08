c4a3a1116d613d80362e945a5160c8800f3fa065
Enver Haase <enver@vaadin.com>
2021-09-09 00:05:45 +0200

    Revert "Developed further -- still no proper hook for building an object tree."
    This reverts commit f8a0d46ffb8f31805129c0fac4752e75fe0945cc.

--- learningfusion/src/main/java/com/example/application/views/helloworldflow/HelloWorldFlowView.java
+++ learningfusion/src/main/java/com/example/application/views/helloworldflow/HelloWorldFlowView.java
@@ -1,27 +1,21 @@
 package com.example.application.views.helloworldflow;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.WrappedSession;
+import org.openjdk.jol.info.GraphLayout;
 
-import org.ehcache.sizeof.SizeOf;
-import org.ehcache.sizeof.VisitorListener;
-import org.ehcache.sizeof.filters.SizeOfFilter;
-
-
-import java.lang.reflect.Field;
-import java.util.Collection;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Route(value = "hello-world-flow")
 @PageTitle("Hello-World-Flow")
@@ -33,50 +27,34 @@ public class HelloWorldFlowView extends HorizontalLayout {
 
     private final Set<byte[]> bytes = new HashSet<>();
 
-    Logger logger = Logger.getLogger(HelloWorldFlowView.class.getName());
-
-    private class DependencyWalker implements SizeOfFilter{
-        @Override
-        public Collection<Field> filterFields(Class<?> aClass, Collection<Field> collection) {
-            logger.log(Level.INFO, "Visiting "+collection.size()+" field(s) of class "+aClass.getName()+":");
-            for (Field field : collection){
-                logger.log(Level.INFO, field.getType().getName()+" --- "+field.getName());
-            }
-            logger.log(Level.INFO, "Done visiting "+collection.size()+" field(s) of class "+aClass.getName());
-            return collection;
-        }
-
-        @Override
-        public boolean filterClass(Class<?> aClass) {
-            logger.log(Level.INFO, "Visiting class "+aClass.getName());
-            return true;
-        }
-    }
-
-    private class MyVisitorListener implements VisitorListener {
-        private long currentDepth;
-        private long currentSize;
-        @Override
-        public void visited(Object o, long size) {
-            currentDepth += 1;
-            currentSize += size;
-            logger.log(Level.INFO, "Depth "+currentDepth+", currentSize "+currentSize+" --- Object "+System.identityHashCode(o)+" of class "+o.getClass().getName()+" visited, size "+size);
-        }
-    }
-
     public HelloWorldFlowView() {
         addClassName("hello-world-flow-view");
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
-        TreeGrid treeGrid = new TreeGrid<>();
-        add(name, sayHello, treeGrid);
+        add(name, sayHello);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue()+"! Session size is: "+SizeOf.newInstance(false, false, new DependencyWalker()).deepSizeOf(new MyVisitorListener(), VaadinSession.getCurrent()));
+            Notification.show("Hello " + name.getValue()+"! Session size is: "+getSessionSize()+" / "+GraphLayout.parseInstance(VaadinSession.getCurrent()).totalSize()+".");
 
             bytes.add(new byte[10000]);
         });
     }
 
+    private long getSessionSize(){
+        try {
+            VaadinSession vaadinSession = VaadinSession.getCurrent();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(vaadinSession);
+            oos.close();
+
+            return baos.size();
+        }
+        catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+        return 0;
+    }
 
 }
