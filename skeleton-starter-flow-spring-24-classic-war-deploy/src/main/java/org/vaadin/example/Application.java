9935a8401f866022b76961aa743be82718a23535
Enver Haase (vaadin) <enver@vaadin.com>
2025-02-01 16:54:18 +0100

    Add a new starter project for Vaadin with Spring Boot but no embedded Tomcat server.

---
+++ skeleton-starter-flow-spring-24-classic-war-deploy/src/main/java/org/vaadin/example/Application.java
@@ -0,0 +1,31 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@Theme("my-theme")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
